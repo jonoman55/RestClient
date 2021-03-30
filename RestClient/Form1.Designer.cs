b50e9754f0cdb75d7b9856d9c6b39b48fe510281
jonoman55 <jonoman55@yahoo.com>
2021-03-30 11:25:32 -0400

    --Added _RestClient.cs

--- RestClient/Form1.Designer.cs
+++ RestClient/Form1.Designer.cs
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textRequest = new System.Windows.Forms.TextBox();
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.cmdBtn = new System.Windows.Forms.Button();
+            this.reqlLabel = new System.Windows.Forms.Label();
+            this.respLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textRequest
+            // 
+            this.textRequest.Location = new System.Drawing.Point(173, 38);
+            this.textRequest.Name = "textRequest";
+            this.textRequest.Size = new System.Drawing.Size(268, 20);
+            this.textRequest.TabIndex = 0;
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResponse.Location = new System.Drawing.Point(173, 74);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResponse.Size = new System.Drawing.Size(410, 169);
+            this.txtResponse.TabIndex = 1;
+            // 
+            // cmdBtn
+            // 
+            this.cmdBtn.Location = new System.Drawing.Point(447, 38);
+            this.cmdBtn.Name = "cmdBtn";
+            this.cmdBtn.Size = new System.Drawing.Size(75, 23);
+            this.cmdBtn.TabIndex = 2;
+            this.cmdBtn.Text = "Go";
+            this.cmdBtn.UseVisualStyleBackColor = true;
+            this.cmdBtn.Click += new System.EventHandler(this.cmdBtn_Click);
+            // 
+            // reqlLabel
+            // 
+            this.reqlLabel.AutoSize = true;
+            this.reqlLabel.Location = new System.Drawing.Point(92, 41);
+            this.reqlLabel.Name = "reqlLabel";
+            this.reqlLabel.Size = new System.Drawing.Size(75, 13);
+            this.reqlLabel.TabIndex = 3;
+            this.reqlLabel.Text = "Request URL:";
+            // 
+            // respLabel
+            // 
+            this.respLabel.AutoSize = true;
+            this.respLabel.Location = new System.Drawing.Point(92, 77);
+            this.respLabel.Name = "respLabel";
+            this.respLabel.Size = new System.Drawing.Size(58, 13);
+            this.respLabel.TabIndex = 4;
+            this.respLabel.Text = "Response:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 268);
+            this.Controls.Add(this.respLabel);
+            this.Controls.Add(this.reqlLabel);
+            this.Controls.Add(this.cmdBtn);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.textRequest);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textRequest;
+        private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.Button cmdBtn;
+        private System.Windows.Forms.Label reqlLabel;
+        private System.Windows.Forms.Label respLabel;
     }
 }
 
--- RestClient/Form1.cs
+++ RestClient/Form1.cs
@@ -16,5 +16,27 @@ namespace RestClient
         {
             InitializeComponent();
         }
+
+        #region UI Event Modules
+        private void cmdBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        private void debugOutput(string strDebugText)
+        {
+            try
+            {
+                System.Diagnostics.Debug.Write(strDebugText + Environment.NewLine);
+                txtResponse.Text = txtResponse.Text + strDebugText + Environment.NewLine;
+                txtResponse.SelectionStart = txtResponse.TextLength;
+                txtResponse.ScrollToCaret();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex.Message, ToString() + Environment.NewLine);
+            }
+        }
     }
 }
